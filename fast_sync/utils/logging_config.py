87c8e66b3e8b15be96feaa980a3a2b3dcbebc4e4
Daniil <72316169+jirobassik@users.noreply.github.com>
2026-03-01 18:37:04 +0300

    Add: platformdirs find log folder
    * Add: platformdirs find log folder
    
    * Update: default prod without env file

--- fast_sync/utils/logging_config.py
+++ fast_sync/utils/logging_config.py
@@ -1,10 +1,18 @@
 import os
 import sys
+from pathlib import Path
 
 from dotenv import load_dotenv
 from loguru import logger
+from platformdirs import PlatformDirs
+
+from fast_sync.utils.constant import APP_NAME
 
 load_dotenv()
+
+default_path_to_log: Path = PlatformDirs(
+    appname=APP_NAME, appauthor=False
+).user_log_path
 
 
 def setup_logging():
@@ -17,14 +25,14 @@ def setup_logging():
     )
     prod_format = "{time:YYYY-MM-DD HH:mm:ss} | {level} | {name}:{function} | {message}"
 
-    if os.getenv("ENV_TYPE", "debug") == "debug":
+    if os.getenv("ENV_TYPE", "prod") == "debug":
         logger.add(sys.stderr, level="DEBUG", format=dev_format, colorize=True)
         logger.info("Development mode: logging is configured to output to the console.")
     else:
         logger.add(sys.stderr, level="INFO", format=prod_format, colorize=False)
 
         logger.add(
-            "logs/app.log",
+            f"{default_path_to_log}/app.log",
             level="DEBUG",
             rotation="10 MB",
             retention="1 month",
@@ -32,5 +40,5 @@ def setup_logging():
             serialize=True,
         )
         logger.info(
-            "Production mode: logging is configured to output to console and file."
+            f"Production mode: logging is configured to output to file and console.\nPath to log file: {default_path_to_log}"
         )
