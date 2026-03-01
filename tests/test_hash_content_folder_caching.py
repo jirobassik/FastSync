7a9f1ab160a0c3ee6d493c78381ae1c702a0084c
Daniil <72316169+jirobassik@users.noreply.github.com>
2026-03-01 13:11:19 +0300

    Update: platformdirs for find cache folder

--- tests/test_hash_content_folder_caching.py
+++ tests/test_hash_content_folder_caching.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 
 from fast_sync import HashContentFolderCaching
+from fast_sync.utils.constant import APP_NAME
 
 
 def test_creation_cache_file(tmp_path, cache_folder_creation):
-    expected_path = tmp_path / "FastSync" / "cache.db"
+    expected_path = tmp_path / APP_NAME / "cache.db"
     actual_path = Path(
         HashContentFolderCaching(
             cache_path=cache_folder_creation(tmp_path)
