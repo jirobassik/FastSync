de6e000f52b249a473b10f3a8f0ab208c8befbb6
jirobassik <jirobassik@tut.by>
2026-03-24 18:46:55 +0300

    Fix: test folder_structure_hash fixture

--- tests/fixtures/fixtures.py
+++ tests/fixtures/fixtures.py
@@ -1,4 +1,4 @@
-from pathlib import PosixPath
+from pathlib import Path
 
 import pytest
 
@@ -25,35 +25,35 @@ def cache_folder_creation():
 def folder_structure_hash():
     return [
         (
-            ("637a1e7b9756424ca2ad8309a7f50f0b", PosixPath("json/t24.csv")),
-            PosixPath("/home/puzer/OS_emulate/TestEqual/json/t24.csv"),
+            ("637a1e7b9756424ca2ad8309a7f50f0b", Path("json/t24.csv")),
+            Path("/home/puzer/OS_emulate/TestEqual/json/t24.csv"),
         ),
         (
-            ("738f7ca27b9cf853362f28fc16b8101f", PosixPath("json/t1.json")),
-            PosixPath("/home/puzer/OS_emulate/TestEqual/json/t1.json"),
+            ("738f7ca27b9cf853362f28fc16b8101f", Path("json/t1.json")),
+            Path("/home/puzer/OS_emulate/TestEqual/json/t1.json"),
         ),
         (
-            ("c6bc2e91406290bcf0000a1442cc4858", PosixPath("json/t2.json")),
-            PosixPath("/home/puzer/OS_emulate/TestEqual/json/t2.json"),
+            ("c6bc2e91406290bcf0000a1442cc4858", Path("json/t2.json")),
+            Path("/home/puzer/OS_emulate/TestEqual/json/t2.json"),
         ),
         (
-            ("637a1e7b9756424ca2ad8309a7f50f0b", PosixPath("csv/t24.csv")),
-            PosixPath("/home/puzer/OS_emulate/TestEqual/csv/t24.csv"),
+            ("637a1e7b9756424ca2ad8309a7f50f0b", Path("csv/t24.csv")),
+            Path("/home/puzer/OS_emulate/TestEqual/csv/t24.csv"),
         ),
         (
-            ("c6bc2e91406290bcf0000a1442cc4858", PosixPath("csv/t2.json")),
-            PosixPath("/home/puzer/OS_emulate/TestEqual/csv/t2.json"),
+            ("c6bc2e91406290bcf0000a1442cc4858", Path("csv/t2.json")),
+            Path("/home/puzer/OS_emulate/TestEqual/csv/t2.json"),
         ),
         (
-            ("637a1e7b9756424ca2ad8309a7f50f0b", PosixPath("csv/t2.csv")),
-            PosixPath("/home/puzer/OS_emulate/TestEqual/csv/t2.csv"),
+            ("637a1e7b9756424ca2ad8309a7f50f0b", Path("csv/t2.csv")),
+            Path("/home/puzer/OS_emulate/TestEqual/csv/t2.csv"),
         ),
         (
-            ("d600008f9526d19de7c55e2183ad6ad0", PosixPath("pickle/t2.pickle")),
-            PosixPath("/home/puzer/OS_emulate/TestEqual/pickle/t2.pickle"),
+            ("d600008f9526d19de7c55e2183ad6ad0", Path("pickle/t2.pickle")),
+            Path("/home/puzer/OS_emulate/TestEqual/pickle/t2.pickle"),
         ),
         (
-            ("8fa6d1b3de9969b2925c338d3a25554e", PosixPath("pickle/t1.pickle")),
-            PosixPath("/home/puzer/OS_emulate/TestEqual/pickle/t1.pickle"),
+            ("8fa6d1b3de9969b2925c338d3a25554e", Path("pickle/t1.pickle")),
+            Path("/home/puzer/OS_emulate/TestEqual/pickle/t1.pickle"),
         ),
     ]
